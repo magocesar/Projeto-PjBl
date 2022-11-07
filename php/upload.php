e33af801c13b09302210fee0b51baf9006d29125
OtvCng <otavionogueira@hotmail.com>
2022-11-07 12:25:02 -0300

    COMO Q CENTRALIZA UMA DIV AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
    aderi a frescura do cesinha

---
+++ php/upload.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>upload</title>
+  <link rel="stylesheet" href="css/upload.css">
+</head>
+<body>
+  
+  <form action="" method="POST" class='uploadForm'>
+    <input type="text" name="title" id="title" class='title' placeholder='Adicione um Titulo' required> <br> <br>
+    <input type="file" name="fileUpload" id="fileUpload" class='fileUpload' required> <br> <br>
+    <textarea name="description" id="description" cols="20" rows="10" placeholder='Adicione uma descricao' required></textarea>
+  </form>
+
+</body>
+</html>
