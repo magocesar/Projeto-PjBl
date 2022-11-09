0d2225992e8bb574d930f6dbb47d0bc7062e8edc
OtvCng <otavionogueira@hotmail.com>
2022-11-09 14:33:45 -0300

    1 stylesheet e mudanca na sidebar

--- frescura_cesinha/areaDeBusca.php
+++ frescura_cesinha/areaDeBusca.php
@@ -5,7 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>areaDeBusca</title>
-  <link rel="stylesheet" href="css/homepage.css">
+  <link rel="stylesheet" href="css/areaDoProfessor.css">
 </head>
 <body>
 <div class="container">
--- frescura_cesinha/conta.php
+++ frescura_cesinha/conta.php
@@ -15,7 +15,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Minha Conta</title>
-    <link rel="stylesheet" href="css/conta.css">
+    <link rel="stylesheet" href="css/areaDoProfessor.css">
     <script src="conta.js"></script>
 </head>
 <body>
