884dde8ef57a61e23785ccc8f50c30b19d8c0847
Gabriela Ribeiro <gr08.ribeiro@gmail.com>
2022-11-08 22:52:30 -0300

    b

--- php/homepage.php
+++ php/homepage.php
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div class="app-header-right">
-      <a href="upload.php"> <button class="add-btn" title="Add New Project"'> </a>
+      <a href="upload.php"> <button class="add-btn" title="Add New Project"> </a>
         <svg class="btn-icon" xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-plus">
           <line x1="12" y1="5" x2="12" y2="19" />
           <line x1="5" y1="12" x2="19" y2="12" /></svg>
