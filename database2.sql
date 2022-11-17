9c48616e1662ba50f8f59e22f278ae2c3f939ee1
César Willian Pacheco <cwillianpacheco@gmail.com>
2022-11-17 16:26:06 -0300

    Update database2.sql

--- database2.sql
+++ database2.sql
@@ -56,6 +56,30 @@ ALTER TABLE professores
 	MODIFY COLUMN cod_professor INT
 	DEFAULT 2;
 
-
+INSERT INTO alunos (nome_aluno, login_aluno, senha_aluno) VALUES
+	('César', 'César', '22'),
+    ('Rodrigo', 'Rodrigo', '22'),
+    ('Gabriela', 'Gabriela', '22'),
+    ('Hack', 'Hack', '22'),
+    ('Otávio', 'Otávio', '22');
     
+INSERT INTO professores (nome_professor, login_professor, senha_professor) VALUES 
+	('Cristina', 'Cristina', '22'),
+    ('Maicris', 'Maicris', '22'),
+    ('Guilherme', 'Guilherme', '22'),
+    ('Rafaela', 'Rafaela', '22'),
+    ('Sheila', 'Sheila', '22');
+    
+INSERT INTO cursos (nome_curso, descricao_curso, materia_curso, id_professor_curso) VALUES
+	('Banco de Dados', 'Curso de Banco de Dados', 'Programação', 1),
+    ('Programação WEB', 'Curso de Programação WEB', 'Programação', 2),
+    ('Conectividade em Sistemas Cyberfísicos', 'Curso de Conectividade em Sistemas Cyberfísicos', 'Desenvolvimento de Redes', 3),
+    ('Lógica Matemática', 'Curso de Lógica Matemática', 'Lógica Matemática', 4),
+    ('Diagramas UML', 'Curso de Diagramas UML', 'Curso UML', 5);
 
+INSERT INTO alunos_cursos (aluno, curso) VALUES
+	( 1, 1),
+    ( 2, 2),
+    ( 3, 3),
+    ( 4, 4),
+    ( 5, 5)
